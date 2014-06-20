02d5c1c67892b9cdac3b29e82c6aff6d092bc7a2
jonavargas <jonavargas2012@gmail.com>
2014-06-19 23:53:36 -0600

    Search Employee

---
+++ POS/POS/Controllers/SearchEmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibrary1;
+using POS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POS.Controllers
+{
+    public class SearchEmployeeController : Controller
+    {
+        private POS_DB db = new POS_DB();
+        //
+        // GET: /SearchEmployee/
+
+        public ActionResult Index()
+        {
+            var employee = db.Employee.ToList();
+            var model = new SearchEmployeeModels();
+            model.Employee = employee;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SearchEmployeeModels model)
+        {
+            var search = model.SearchText;
+
+            if (string.IsNullOrEmpty(search))
+            {
+                model.Employee = db.Employee.ToList();
+            }
+            else
+            {
+                // fill restaurants in model
+                model.Employee = db.Employee.Where(e => e.name.Contains(search)).ToList();
+            }
+
+            // return model to the view
+            return View(model);
+        }
+    }
+}
---
+++ POS/POS/Models/SearchEmployeeModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POS.Models
+{
+    public class SearchEmployeeModels
+    {
+        public string SearchText { get; set; }
+        public List<ClassLibrary1.Employee> Employee { get; set; } 
+    }
+}
